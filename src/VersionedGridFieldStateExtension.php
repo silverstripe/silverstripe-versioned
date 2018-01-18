4b8140e15e59a88b5908b6beb1523df7ca643c20
Damian Mooyman <damian@silverstripe.com>
2018-01-18 17:58:21 +1300

    API Refactor GridFieldVersionedState into versioned module

---
+++ src/VersionedGridFieldStateExtension.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace SilverStripe\Versioned;
+
+use SilverStripe\Core\Extension;
+use SilverStripe\Forms\GridField\GridFieldConfig;
+use SilverStripe\Versioned\VersionedGridFieldState\VersionedGridFieldState;
+
+/**
+ * Decorates a GridFieldConfig with gridfield publishing state
+ */
+class VersionedGridFieldStateExtension extends Extension
+{
+    public function updateConfig()
+    {
+        /** @var GridFieldConfig $owner */
+        $owner = $this->getOwner();
+        $owner->addComponent(new VersionedGridFieldState());
+    }
+}
