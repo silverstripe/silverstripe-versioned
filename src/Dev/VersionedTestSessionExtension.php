d35d7b9905157b9457159fce4d88ef5cc201f09b
Damian Mooyman <damian@silverstripe.com>
2018-03-21 17:35:48 +1300

    Set unsecured draft site as well.

--- src/Dev/VersionedTestSessionExtension.php
+++ src/Dev/VersionedTestSessionExtension.php
@@ -8,7 +8,8 @@ use SilverStripe\Versioned\Versioned;
 use SilverStripe\Versioned\VersionedStateExtension;
 
 /**
- * Decorates TestSession object to update get / post requests with versioned querystring arguments
+ * Decorates TestSession object to update get / post requests with versioned querystring arguments.
+ * Session vars assigned by FunctionalTest::useDraftSite are respected here.
  *
  * @property TestSession $owner
  */
@@ -25,16 +26,21 @@ class VersionedTestSessionExtension extends VersionedStateExtension
         if (!$session) {
             return;
         }
-        $readingMode = $session->get('readingMode');
-        if (!$readingMode) {
-            return;
+
+        // Set unsecured draft
+        $unsecuredDraft = $session->get('unsecuredDraftSite');
+        if (isset($unsecuredDraft)) {
+            Versioned::set_draft_site_secured(!$unsecuredDraft);
         }
 
-        // Set to reading mode, decorate link
-        Versioned::withVersionedMode(function () use ($readingMode, &$url) {
-            Versioned::set_reading_mode($readingMode);
-            $this->updateLink($url);
-        });
+        // Set reading mode
+        $readingMode = $session->get('readingMode');
+        if ($readingMode) {
+            Versioned::withVersionedMode(function () use ($readingMode, &$url) {
+                Versioned::set_reading_mode($readingMode);
+                $this->updateLink($url);
+            });
+        }
     }
 
     /**
