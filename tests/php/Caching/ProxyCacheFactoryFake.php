45bbeb5832ca2d7fc9204c7b42b42223549aeb82
Aaron Carlino <unclecheese@leftandmain.com>
2018-05-24 16:21:29 +1200

    Factory tests

---
+++ tests/php/Caching/ProxyCacheFactoryFake.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace SilverStripe\Versioned\Tests\Caching;
+
+use SilverStripe\Versioned\Caching\ProxyCacheFactory;
+
+class ProxyCacheFactoryFake extends ProxyCacheFactory
+{
+    protected function isAPCUSupported()
+    {
+        return false;
+    }
+
+    protected function isPHPFilesSupported()
+    {
+        return true;
+    }
+
+}
---
+++ tests/php/Caching/ProxyCacheFactoryTest.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace SilverStripe\Versioned\Tests\Caching;
+
+use SilverStripe\Dev\SapphireTest;
+use Symfony\Component\Cache\Simple\PhpFilesCache;
+
+class ProxyCacheFactoryTest extends SapphireTest
+{
+    public function testCreateFallback()
+    {
+        $factory = new ProxyCacheFactoryFake([
+            'argOne' => 'one'
+        ]);
+        $result = $factory->create('dummy', ['argTwo' => 'two']);
+
+        $this->assertTrue($result instanceof PhpFilesCache);
+    }
+
+    public function testCreateCustomContainer()
+    {
+        $factory = new ProxyCacheFactoryFake([
+            'argOne' => 'one',
+            'container' => ProxyCacheAdapterFake::class,
+        ]);
+        $result = $factory->create('dummy', []);
+        $this->assertTrue($result instanceof ProxyCacheAdapterFake);
+
+        $factory = new ProxyCacheFactoryFake([
+            'argOne' => 'one',
+        ]);
+        $result = $factory->create('dummy', ['container' => ProxyCacheAdapterFake::class]);
+        $this->assertTrue($result instanceof ProxyCacheAdapterFake);
+    }
+
+    public function testDisableContainer()
+    {
+        $factory = new ProxyCacheFactoryFake([
+            'argOne' => 'one',
+            'container' => ProxyCacheAdapterFake::class,
+        ]);
+        $result = $factory->create('dummy', ['disable-container' => true]);
+        $this->assertTrue($result instanceof PhpFilesCache);
+
+    }
+}
