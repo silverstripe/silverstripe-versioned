34e7bed2d74531ef66b5287d2eeaac382132f7eb
Aaron Carlino <unclecheese@leftandmain.com>
2018-05-31 09:06:18 +1200

    Update tests to more semantic keys

--- tests/php/Caching/VersionedCacheTest.php
+++ tests/php/Caching/VersionedCacheTest.php
@@ -26,19 +26,19 @@ class VersionedCacheTest extends SapphireTest
         $cacheInstance->clear();
 
         Versioned::set_stage(Versioned::LIVE);
-        $result = $cacheInstance->get('test');
+        $result = $cacheInstance->get('shared_key');
         $this->assertNull($result);
-        $cacheInstance->set('test', 'uncle');
-        $this->assertEquals('uncle', $cacheInstance->get('test'), 'Value is cached on LIVE');
+        $cacheInstance->set('shared_key', 'uncle');
+        $this->assertEquals('uncle', $cacheInstance->get('shared_key'), 'Shared key is cached on LIVE');
         Versioned::set_stage(Versioned::DRAFT);
-        $this->assertNull($cacheInstance->get('test'), 'Value is not cached on DRAFT');
-        $cacheInstance->set('test', 'cheese');
-        $cacheInstance->set('foo', 'bar');
-        $this->assertEquals('cheese', $cacheInstance->get('test'), 'Value of same key is cached on DRAFT');
-        $this->assertEquals('bar', $cacheInstance->get('foo'), 'Value of different key is cached on DRAFT');
+        $this->assertNull($cacheInstance->get('shared_key'), 'Shared key is not cached on DRAFT');
+        $cacheInstance->set('shared_key', 'cheese');
+        $cacheInstance->set('draft_key', 'bar');
+        $this->assertEquals('cheese', $cacheInstance->get('shared_key'), 'Shared key has its own value on DRAFT');
+        $this->assertEquals('bar', $cacheInstance->get('draft_key'), 'Draft-only key is cached on DRAFT');
         Versioned::set_stage(Versioned::LIVE);
-        $this->assertNull($cacheInstance->get('foo'), 'Value of different key is not on LIVE');
-        $this->assertEquals('uncle', $cacheInstance->get('test'), 'Value of same key is preserved on LIVE');
+        $this->assertNull($cacheInstance->get('draft_key'), 'Draft-only key is not on LIVE');
+        $this->assertEquals('uncle', $cacheInstance->get('shared_key'), 'Value of shared key is preserved on LIVE');
 
         $cacheInstance->clear();
     }
