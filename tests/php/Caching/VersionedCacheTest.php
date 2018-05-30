03da96aef565ee7b061f8749cef3a83fc38fc052
Aaron Carlino <unclecheese@leftandmain.com>
2018-05-30 15:16:47 +1200

    add test descriptions

--- tests/php/Caching/VersionedCacheTest.php
+++ tests/php/Caching/VersionedCacheTest.php
@@ -29,16 +29,16 @@ class VersionedCacheTest extends SapphireTest
         $result = $cacheInstance->get('test');
         $this->assertNull($result);
         $cacheInstance->set('test', 'uncle');
-        $this->assertEquals('uncle', $cacheInstance->get('test'));
+        $this->assertEquals('uncle', $cacheInstance->get('test'), 'Value is cached on LIVE');
         Versioned::set_stage(Versioned::DRAFT);
-        $this->assertNull($cacheInstance->get('test'));
+        $this->assertNull($cacheInstance->get('test'), 'Value is not cached on DRAFT');
         $cacheInstance->set('test', 'cheese');
         $cacheInstance->set('foo', 'bar');
-        $this->assertEquals('cheese', $cacheInstance->get('test'));
-        $this->assertEquals('bar', $cacheInstance->get('foo'));
+        $this->assertEquals('cheese', $cacheInstance->get('test'), 'Value of same key is cached on DRAFT');
+        $this->assertEquals('bar', $cacheInstance->get('foo'), 'Value of different key is cached on DRAFT');
         Versioned::set_stage(Versioned::LIVE);
-        $this->assertNull($cacheInstance->get('foo'));
-        $this->assertEquals('uncle', $cacheInstance->get('test'));
+        $this->assertNull($cacheInstance->get('foo'), 'Value of different key is not on LIVE');
+        $this->assertEquals('uncle', $cacheInstance->get('test'), 'Value of same key is preserved on LIVE');
 
         $cacheInstance->clear();
     }
