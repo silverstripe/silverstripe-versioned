dc59bd8e5613442b214f951ca16ec376e1ee1cda
Robbie Averill <robbie@silverstripe.com>
2018-09-25 18:27:29 +0200

    FIX Published GraphQL field now correctly indicates whether the record's version is published

--- src/GraphQL/Extensions/DataObjectScaffolderExtension.php
+++ src/GraphQL/Extensions/DataObjectScaffolderExtension.php
@@ -55,7 +55,7 @@ class DataObjectScaffolderExtension extends Extension
                     'Published' => [
                         'type' => Type::boolean(),
                         'resolve' => function ($obj) {
-                            return $obj->isPublished();
+                            return $obj->WasPublished;
                         }
                     ],
                     'LiveVersion' => [
