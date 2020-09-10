fc9bf93635db4e228cbdbbc3e38b36c93faee4cc
Mojmir Fendek <mfendek@silverstripe.com>
2020-09-11 09:42:43 +1200

    BUG: UI related changes moved from Versioned to Versioned Admin.

--- client/src/bundles/bundle.js
+++
@@ -1,8 +0,0 @@
-/* eslint-disable
- import/no-webpack-loader-syntax,
- import/no-extraneous-dependencies,
- import/no-unresolved
- */
-
-// Legacy CMS
-require('../legacy/VersionedEditForm/VersionedEditForm');
