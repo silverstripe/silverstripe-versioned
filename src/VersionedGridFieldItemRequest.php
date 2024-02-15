3065c132fcc0214cc30196c6d02e8f6cd6013ced
Thomas Portelange <thomas@lekoala.be>
2024-02-15 05:59:29 +0100

    properly encode X-Status

--- src/VersionedGridFieldItemRequest.php
+++ src/VersionedGridFieldItemRequest.php
@@ -26,6 +26,10 @@ use SilverStripe\View\SSViewer;
  */
 class VersionedGridFieldItemRequest extends GridFieldDetailForm_ItemRequest
 {
+    /**
+     * @param bool $unlinked
+     * @return ArrayList
+     */
     public function Breadcrumbs($unlinked = false)
     {
         $items = parent::Breadcrumbs($unlinked);
@@ -42,6 +46,7 @@ class VersionedGridFieldItemRequest extends GridFieldDetailForm_ItemRequest
         $this->extend('updateBadge', $badge);
 
         if ($badge) {
+            /** @var ArrayData $lastItem */
             $lastItem = $items->last();
             $lastItem->setField('Extra', $badge);
         }
@@ -131,7 +136,7 @@ class VersionedGridFieldItemRequest extends GridFieldDetailForm_ItemRequest
             'Archived {name} "{title}"',
             [
                 'name' => $record->i18n_singular_name(),
-                'title' => Convert::raw2xml($title)
+                'title' => $title
             ]
         );
         $this->setFormMessage($form, $message);
@@ -139,7 +144,7 @@ class VersionedGridFieldItemRequest extends GridFieldDetailForm_ItemRequest
         //when an item is deleted, redirect to the parent controller
         $controller = $this->getToplevelController();
         $controller->getRequest()->addHeader('X-Pjax', 'Content'); // Force a content refresh
-        $controller->getResponse()->addHeader('X-Status', $message);
+        $controller->getResponse()->addHeader('X-Status', rawurlencode($message));
         return $controller->redirect($this->getBackLink(), 302); //redirect back to admin section
     }
 
@@ -183,12 +188,12 @@ class VersionedGridFieldItemRequest extends GridFieldDetailForm_ItemRequest
             'Published {type} "{title}"',
             [
                 'type' => $record->i18n_singular_name(),
-                'title' => Convert::raw2xml($record->Title)
+                'title' => $record->Title
             ]
         );
 
         $controller = $this->getToplevelController();
-        $controller->getResponse()->addHeader('X-Status', $message);
+        $controller->getResponse()->addHeader('X-Status', rawurlencode($message));
 
         return $this->redirectAfterSave($isNewRecord);
     }
@@ -217,13 +222,13 @@ class VersionedGridFieldItemRequest extends GridFieldDetailForm_ItemRequest
             'Unpublished {name} "{title}"',
             [
                 'name' => $record->i18n_singular_name(),
-                'title' => Convert::raw2xml($title)
+                'title' => $title
             ]
         );
         $this->setFormMessage($form, $message);
 
         $controller = $this->getToplevelController();
-        $controller->getResponse()->addHeader('X-Status', $message);
+        $controller->getResponse()->addHeader('X-Status', rawurlencode($message));
 
         // Redirect back to edit
         return $this->redirectAfterSave(false);
